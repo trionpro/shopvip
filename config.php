c58d2254ad329945bf9c922961313ee6e7d1594a
Min <trionnguyen@hotmail.com>
2024-05-01 21:21:27 -0700

    fix run in home

--- config.php
+++ config.php
@@ -5,12 +5,14 @@ $config = array(
     /*** Database Config ***/
 'LOCALHOST' => 'localhost', // mysql host service
 'USERNAME' => 'root', // username
-'PASSWORD' => 'Kd@123@', // password
+//'PASSWORD' => 'Kd@123@', // password
+'PASSWORD' => '', // password
 'DATABASE' => 'db_shopvip', // database name
 
 'facebook_app_id' => 'null',
 'facebook_app_key' => 'null',
-'home' => 'http://localhost:81/shopvip', // url website có ssl và không có / ở cuối
+//'home' => 'http://localhost:81/shopvip', // url website có ssl và không có / ở cuối
+'home' => 'http://localhost/shopvip', // url website có ssl và không có / ở cuối
 
     /*** Gạch Thẻ Config ***/   
 'CALLBACK_URL' => 'http://'.$_SERVER['SERVER_NAME'].'/model/card_callback.php', // Không thay đổi
